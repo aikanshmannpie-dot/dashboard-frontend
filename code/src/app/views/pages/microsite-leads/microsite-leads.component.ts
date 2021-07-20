efa41e6e97cbbb09383420106e0e2680279b406d
Sahil Arora <sahil.arora@pieinfosystems.com>
2021-07-20 09:21:08 +0530

    add sorting to microsite lead name

--- code/src/app/views/pages/microsite-leads/microsite-leads.component.ts
+++ code/src/app/views/pages/microsite-leads/microsite-leads.component.ts
@@ -215,8 +215,8 @@ export class MicrositeLeadsComponent implements OnInit {
 			(data) => {
 				if (data) {
 					console.log("data +++_-=_+", data.data);
-
-					this.siteListingData = data.data;
+					
+					this.siteListingData = data.data.sort((a,b) => a.name.localeCompare(b.name));
 				} else {
 				}
 			},
