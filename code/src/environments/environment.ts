64ce55cd864032b6b7b200f001d291177553f8b6
Alice Abide <acqalice9@gmail.com>
2021-08-28 10:47:27 +0530

    done

--- code/src/environments/environment.ts
+++ code/src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   isMockEnabled: true, // You have to switch this, when your real back-end is done
   authTokenKey: "authce9d77b308c149d5992a80073637e4d5",
-  baseUrl: "http://localhost:8034/api",
-  //baseUrl: "http://54.206.206.214:8034/api",
+  //baseUrl: "http://localhost:8034/api",
+  baseUrl: "http://54.206.206.214:8034/api",
 };
 
 /*
