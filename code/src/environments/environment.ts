63213aa9a4efb71e8ec48e9d60841fd7b9f9e978
mann-aikansh <aikanshmann@gmail.com>
2025-02-13 09:58:11 +0530

    url changed

--- code/src/environments/environment.ts
+++ code/src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   isMockEnabled: true, // You have to switch this, when your real back-end is done
   authTokenKey: "authce9d77b308c149d5992a80073637e4d5",
-  baseUrl: "http://localhost:8034/api",
-  // baseUrl: "http://13.237.196.237:8034/api",
+  // baseUrl: "http://localhost:8034/api",
+  baseUrl: "http://13.237.196.237:8034/api",
   micrositeApiUrl: "https://helathinsurancefind.com.au/api"
 };
 
